818b12fad67a5ec99adab78b313b29cf13274033
mrrobot911 <kelwin2705@gmail.com>
2023-06-27 21:15:29 +0300

    feat: add picture and typing

--- src/scripts/Hero.jsx
+++ src/scripts/Hero.jsx
@@ -1,8 +1,20 @@
+import TextTyper from "./TextTyper.jsx"
 
 const Hero = () => {
+
+  const textToRender = ['This is code for one', 'This is code for two']
+  
   return (
-    <section>
-        <h1>Hi, my name KelWin!</h1>
+    <section className="hero__section">
+        <div className="hero__container">
+          <div className="wrap">
+            <img className="circle"/>
+          </div>
+          <h1>Hi, my name KelWin!</h1>
+        </div>
+        <div className="hero__container">
+          <TextTyper text={textToRender[0]} interval={50} Markup={"p"}/>
+        </div>
     </section>
   )
 }
