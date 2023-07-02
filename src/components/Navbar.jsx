3f3c7e054e56c9bb087b92dd187ec9192a8e6939
mrrobot911 <kelwin2705@gmail.com>
2023-07-02 12:30:58 +0300

    feat: burger start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Context, themes } from "../utils/Context"
 import Toggle from "./Toggle"
 import useMyContext from "../hooks/useMyContext";
@@ -8,14 +8,49 @@ const Navbar = () => {
   const { context } = useMyContext();
   const [toggle, setToggle] = useState(window?.localStorage?.getItem('theme') === 'dark');
   const [value, setValue] = useState(window?.localStorage?.getItem('local') === 'ru'&& window.navigator.language.slice(0,2) === 'ru' ? 2 : 1);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const clickHandler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
 
   function chengeValue(event) {
     setValue(event.target.value);
  }
 
+  useEffect(() => {
+    const handleResize = (event) => {
+      setWidth(event.target.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <nav>
-      <div className="nav__container">
+    <nav className="navbar">
+      { width <= 768 && <div className="burger__container">
+        <button
+          className={isMenuOpen ? "burger__btn change" : "burger__btn"}
+          aria-label="Открыть главное меню"
+          onClick={()=>clickHandler()}
+        > 
+          <div id="bar1" className="bar"></div>
+          <div id="bar2" className="bar"></div>
+          <div id="bar3" className="bar"></div>
+        </button>
+          <div className={isMenuOpen ? "burgerLink__container" : "burgerLink__container show"}>
+            <a href="#about">{locale[context.local]["about"]}</a>
+            <a href="#stuck">{locale[context.local]["stuck"]}</a>
+            <a href="#projects">{locale[context.local]["project"]}</a>
+            <a href="#skills">{locale[context.local]["skill"]}</a>
+          </div>
+          <div className="menu-bg"></div>
+        </div>
+    }
+      {width > 768 && <div className="nav__container">
         <div className="navLink__container">
           <a href="#about">{locale[context.local]["about"]}</a>
         </div>
@@ -28,7 +63,7 @@ const Navbar = () => {
         <div className="navLink__container">
           <a href="#skills">{locale[context.local]["skill"]}</a>
         </div>
-      </div>
+      </div>}
       <div className="theme__container">
         <p>{locale[context.local]["theme"]}</p>
         <Context.Consumer>
